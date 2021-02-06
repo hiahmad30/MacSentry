814cd264cb6c0eb43a0c9e3b5f2679df2048f83f
hiahmad30 <p136015@nu.edu.pk>
2021-02-06 07:01:42 +0500

    update

--- lib/Views/SingUp.dart
+++ lib/Views/SingUp.dart
@@ -1,294 +1,180 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:macsentry/Controllers/AuthController.dart';
+import 'package:macsentry/Views/Login.dart';
 
 import '../constants.dart';
-import 'Login.dart';
+import 'forgetpassword.dart';
 
-class SignupPage extends StatefulWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  _SignupPageState createState() => _SignupPageState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
-  AuthController auth = new AuthController();
-  /////////////////////////////////Controllers////////////////////////////////////////////////////////
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passController = TextEditingController();
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _passConfirmController = TextEditingController();
-///////////////////////////////////Pass Visibility//////////////////////////////
-  bool _passwordVisible1 = false;
-  bool _passwordVisible2 = false;
+class _SignUpPageState extends State<SignUpPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   bool _autoValidate = false;
-  String _name;
-  String _email;
-  String pass;
-  String cpass;
+
+  /////////////////////////////////////GetX Controller///////////////////////////////////
+  AuthController authController = new AuthController();
+
+  /////////////////////////////////Controllers////////////////////////////////////////////////////////
+  TextEditingController _emailControllerlogin = TextEditingController();
+
+  TextEditingController _passControllerlogin = TextEditingController();
+
+///////////////////////////////////Pass Visibility//////////////////////////////
+  bool _passwordVisible1 = true;
+
   /////////////////////////////Validator
+
   bool validator = true;
+
 ///////////////////////////////////Firebase Auth////////////////////////////////////////////////////////
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyResources.backgroundColor,
-      appBar: AppBar(
-        title: Container(
-          child: Text(
-            'Sign up',
-          ),
-        ),
-        centerTitle: true,
-      ),
       body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          autovalidate: _autoValidate,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Center(
-                  child: Text(
-                    "Let's Get Started!",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Center(
-                  child: Text(
-                    "Create an Account to get all features ",
-                    style: GoogleFonts.roboto(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                //Add padding around textfield
-                padding: EdgeInsets.only(top: 25.0, left: 10, right: 10),
-                child: TextFormField(
-                  controller: _nameController, //N
-                  keyboardType: TextInputType.text,
-                  validator: (String arg) {
-                    if (arg.length < 3)
-                      return 'Name must be more than 2 charater';
-                    else
-                      return null;
-                  },
-                  onSaved: (String val) {
-                    _name = val;
-                    print(_name);
-                  }, // ame Controller
-                  decoration: InputDecoration(
-                    //Add th Hint text here.
-                    hintText: "Name",
-                    hintStyle: MyResources.hintfontStyle,
-                    prefixIcon: Icon(Icons.account_circle),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                //Add padding around textfield
-                padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
-                child: TextFormField(
-                  controller: _emailController,
-                  //Email Controller
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (val) {
-                    if (val.length == 0)
-                      return "Please enter email";
-                    else if (!val.contains("@"))
-                      return "Please enter valid email";
-                    else
-                      return null;
-                  },
-                  onSaved: (val) => _email = val,
-
-                  decoration: InputDecoration(
-                    //Add th Hint text here.
-                    hintText: "Email",
-                    hintStyle: MyResources.hintfontStyle,
-                    prefixIcon: Icon(Icons.email),
-                    // errorStyle: TextStyle(fontSize: 6),
-
-                    /*errorText:
-                        GetUtils.isEmail(_emailController.text)? null
-                        : "true",*/
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                //Add padding around textfield
-                padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  obscureText: !_passwordVisible1,
-                  controller: _passController, //// pass Controller
-                  validator: (val) {
-                    if (val.length == 0) {
-                      return ('Please enter password');
-                    } else if (val.length <= 7) {
-                      return ('Your password must be at least 8 characters long.');
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (val) => pass = val,
-                  decoration: InputDecoration(
-                    //Add th Hint text here.
-                    hintText: "Password",
-                    hintStyle: MyResources.hintfontStyle,
-                    suffixIcon: IconButton(
-                        icon: Icon(
-                          // Based on passwordVisible state choose the icon
-                          _passwordVisible1
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: Colors.blue,
-                        ),
-                        onPressed: () {
-                          // Update the state i.e. toogle the state of passwordVisible variable
-                          setState(() {
-                            _passwordVisible1 = !_passwordVisible1;
-                          });
-                        }),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                //Add padding around textfield
-                padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  obscureText: !_passwordVisible2,
-                  controller: _passConfirmController,
-                  validator: (val) {
-                    if (val.length == 0) {
-                      return ('Please enter password');
-                    } else if (val != _passController.text) {
-                      return ('Password not match');
-                    }
-                    return null;
-                  },
-                  onSaved: (val) => cpass = val,
-
-                  //pass Confirmation controller
-                  decoration: InputDecoration(
-                    //Add th Hint text here.
-                    hintText: "Confirm Password",
-                    hintStyle: MyResources.hintfontStyle,
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                        icon: Icon(
-                          // Based on passwordVisible state choose the icon
-                          _passwordVisible2
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: Colors.blue,
-                        ),
-                        onPressed: () {
-                          // Update the state i.e. toogle the state of passwordVisible variable
-                          setState(() {
-                            _passwordVisible2 = !_passwordVisible2;
-                          });
-                        }),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 55.0, left: 10, right: 10),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: 45,
-                  child: RaisedButton(
-                    textColor: Colors.white,
-                    color: Colors.blue,
+        child: SafeArea(
+          child: Form(
+            key: _formKey,
+            autovalidate: _autoValidate,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Center(
                     child: Text(
-                      "SignUp",
+                      "Create Account",
+                      style: MyResources.appHeadingStyle,
                     ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        // Get.to(
-                        //   ProfileEdit(),
-                        // );
-                      }
-                      await auth
-                          .createUser(
-                              _nameController.text,
-                              _emailController.text,
-                              _passConfirmController.text)
-                          .then((value) {
-                        // if ()
-
-                        //  else
-                        Get.snackbar("LoginError",
-                            "User not created . please try again");
-                      });
+                  ),
+                ),
+                Padding(
+                  //Add padding around textfield
+                  padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: _emailControllerlogin,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (val) {
+                      if (val.length == 0)
+                        return "Please enter email";
+                      else if (!val.contains("@"))
+                        return "Please enter valid email";
+                      else
+                        return null;
                     },
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0),
+                    decoration: InputDecoration(
+                      //Add th Hint text here.
+                      hintText: "Email",
+                      hintStyle: MyResources.hintfontStyle,
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Already have an Account?",
-                        style: GoogleFonts.roboto(
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                        ),
+                Padding(
+                  //Add padding around textfield
+                  padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: _passControllerlogin,
+                    validator: (val) {
+                      if (val.length == 0) {
+                        return ('Please enter password');
+                      } else if (val.length <= 7) {
+                        return ('your password must be atleast 7 chracters ');
+                      }
+                      return null;
+                    },
+                    obscureText: _passwordVisible1,
+                    decoration: InputDecoration(
+                      //Add th Hint text here.
+                      hintText: "Password",
+                      hintStyle: MyResources.hintfontStyle,
+                      fillColor: MyResources.loginBtnColor,
+
+                      suffix: InkWell(
+                          child:
+                              // Based on passwordVisible state choose the icon
+                              _passwordVisible1
+                                  ? Container(
+                                      child: Text(
+                                        "Show",
+                                        style: TextStyle(
+                                            color: MyResources.loginBtnColor),
+                                      ),
+                                    )
+                                  : Container(child: Text("Hide")),
+                          onTap: () {
+                            // Update the state i.e. toogle the state of passwordVisible variable
+                            setState(() {
+                              _passwordVisible1 = !_passwordVisible1;
+                            });
+                          }),
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      MaterialButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LoginPage()));
-                          },
-                          child: Text(
-                            " Log in here",
-                            style: GoogleFonts.roboto(
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.blue,
-                            ),
-                          ))
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 15.0, left: 10, right: 10),
+                  child: Container(
+                    width: Get.width,
+                    child: new DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderRadius: BorderRadius.all(10.0),
+                              borderSide: BorderSide(
+                                  color: Colors.grey,
+                                  style: BorderStyle.solid))),
+                      items: <String>['7 Days Free', 'B', 'C', 'D']
+                          .map((String value) {
+                        return new DropdownMenuItem<String>(
+                          value: value,
+                          child: new Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (_) {},
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, left: 10, right: 10),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    height: 50,
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: MyResources.registerBtnColor,
+                      child: Text(
+                        "Create Account",
+                        style: MyResources.appTextStyle,
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          authController.signInwithEmail(
+                              _emailControllerlogin.text,
+                              _passControllerlogin.text);
+
+                          // Get.to();
+                        }
+                      },
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(40.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
