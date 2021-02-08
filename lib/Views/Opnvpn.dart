1d8125eb2ea331269d9a00352dfa3d7a04ae7165
hiahmad30 <p136015@nu.edu.pk>
2021-02-08 09:31:53 +0500

    test openvpn

---
+++ lib/Views/Opnvpn.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_openvpn/flutter_openvpn.dart';
+import 'package:get/get.dart';
+
+import 'opebntest.dart';
+
+class NewPAge extends StatelessWidget {
+  static String subPath = '/page';
+
+  final String page;
+  final bool navigate;
+  NewPAge(this.page, this.navigate);
+  @override
+  Widget build(BuildContext context) {
+    if (page == '0')
+      FlutterOpenvpn.init(
+        localizedDescription: "ExampleVPN",
+        providerBundleIdentifier:
+            "com.topfreelancerdeveloper.flutterOpenvpnExample.RunnerExtension",
+      ).then((value) {
+        print(value);
+        Get.snackbar();
+      });
+    if (page == '2') OVPNPage.initPlatformState();
+    if (navigate) {
+      Future.delayed(Duration(seconds: 3), () {
+        Navigator.of(context)
+            .popAndPushNamed(subPath + (int.parse(page) + 1).toString());
+      });
+    }
+    return Scaffold(
+      body: Center(
+        child: Center(
+          child: Text(
+            page.toString(),
+          ),
+        ),
+      ),
+    );
+  }
+}
