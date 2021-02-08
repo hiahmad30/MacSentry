bd18871939adace50edaef57e99cdfeef2e7e9f9
hiahmad30 <p136015@nu.edu.pk>
2021-02-08 19:02:37 +0500

    --

--- lib/Views/opebntest.dart
+++ lib/Views/opebntest.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_openvpn/flutter_openvpn.dart';
+import 'package:get/get.dart';
+
+import 'Opnvpn.dart';
 
 class OVPNPage extends StatefulWidget {
+  static Future<void> initPlatformState() async {
+    await FlutterOpenvpn.lunchVpn(
+      "https://macsentry.com/config/vie01.macsentry.com.ovpn",
+      (isProfileLoaded) {
+        print('isProfileLoaded : $isProfileLoaded');
+        Get.defaultDialog(
+          title: "Profile Connected",
+          content: Text(isProfileLoaded.toString()),
+        );
+      },
+      (vpnActivated) {
+        // print('vpnActivated : $vpnActivated');
+        Get.defaultDialog(
+            title: "Vpn Connected Connected",
+            content: Text(vpnActivated.toString()));
+      },
+      user: 'pkalos@gmail.com1',
+      pass: 'iQtU0U(3aQ[00d',
+      onConnectionStatusChanged:
+          (duration, lastPacketRecieve, byteIn, byteOut) => print(byteIn),
+      expireAt: DateTime.now().add(
+        Duration(
+          seconds: 180,
+        ),
+      ),
+    );
+  }
+
   @override
   _OVPNPageState createState() => _OVPNPageState();
 }
