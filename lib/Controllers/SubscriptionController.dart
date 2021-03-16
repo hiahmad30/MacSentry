49d409b4c5e62774eacec47e999580f78cb14630
hiahmad30 <p136015@nu.edu.pk>
2021-03-16 18:49:24 +0500

    update

--- lib/Controllers/SubscriptionController.dart
+++ lib/Controllers/SubscriptionController.dart
@@ -1,18 +1,5 @@
 import 'dart:async';
 
 import 'package:get/get.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
 
-class SubscriptionController extends GetxController {
-  StreamSubscription<List<PurchaseDetails>> _subscription;
-
-  @override
-  void onInit() {
-    final Stream purchaseUpdates =
-        InAppPurchaseConnection.instance.purchaseUpdatedStream;
-    _subscription = purchaseUpdates.listen((purchases) {
-      _handlePurchaseUpdates(purchases);
-    });
-    super.onInit();
-  }
-}
+class SubscriptionController extends GetxController {}
